8413455c12787938f8cd1459c7111e83e147cc61
Carlos Melo <carlosmeloti@gmail.com>
2019-09-23 10:42:35 -0300

    app avaliacao

---
+++ src/main/java/br/embrapa/repository/filter/AppAvaliacaoFilter.java
@@ -0,0 +1,17 @@
+package br.embrapa.repository.filter;
+
+public class AppAvaliacaoFilter {
+	
+	private String nmAvaliacao;
+
+	public String getNmAvaliacao() {
+		return nmAvaliacao;
+	}
+
+	public void setNmAvaliacao(String nmAvaliacao) {
+		this.nmAvaliacao = nmAvaliacao;
+	}
+	
+	
+
+}
