9d2fdfc5c3b09f212a4453e3e6d91c60c8a16e70
Carlos Melo <carlosmeloti@gmail.com>
2019-10-22 10:30:12 -0300

    ajustes prod propertys

--- src/main/java/br/embrapa/resource/TokenResource.java
+++ src/main/java/br/embrapa/resource/TokenResource.java
@@ -12,10 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.embrapa.config.property.MopApiProperty;
 
+
 @RestController
 @RequestMapping("/tokens")
 public class TokenResource {
 
+	@Autowired
+	private MopApiProperty mopApiProperty;
 	
+
+	@DeleteMapping("/revoke")
+	public void revoke(HttpServletRequest req, HttpServletResponse resp) {
+		Cookie cookie = new Cookie("refreshToken", null);
+		cookie.setHttpOnly(true);
+		cookie.setSecure(mopApiProperty.getSeguranca().isEnableHttps());
+		cookie.setPath(req.getContextPath() + "/oauth/token");
+		cookie.setMaxAge(0);
+		
+		resp.addCookie(cookie);
+		resp.setStatus(HttpStatus.NO_CONTENT.value());
+	}
 	
 }
