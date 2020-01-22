d71481b0bd05f40510e29d7f930604966e4be1da
unknown <carlosmeloti@gmail.com>
2020-01-22 05:37:29 -0300

    menu empresa

--- src/main/java/br/embrapa/resource/MenuEmpresaResource.java
+++ src/main/java/br/embrapa/resource/MenuEmpresaResource.java
@@ -1,5 +1,17 @@
 package br.embrapa.resource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.embrapa.repository.MenuEmpresaRepository;
+
+@RestController
+@RequestMapping("/menuempresa")
 public class MenuEmpresaResource {
 
+	
+	@Autowired
+	private MenuEmpresaRepository menuEmpresaRepository;
+	
 }
